b5990b1601d14b326099aa0a6e69409bd783a2d6
Tom Ramsden <thomas.ramsden20@gmail.com>
2021-06-13 11:17:06 +0100

    Adjusted element names to better reflect their entities

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
 //Movement Animation to happen
 const card = document.querySelector(".card");
-const container = document.querySelector(".container");
+const cardContainer = document.querySelector(".cardContainer");
 //Items
 const title = document.querySelector(".title");
-const sneaker = document.querySelector(".sneaker img");
-const purchase = document.querySelector(".purchase");
+const photoContainer = document.querySelector(".photoContainer img");
+const cta = document.querySelector(".cta");
 const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
 
 //Moving Animation Event
-container.addEventListener("mousemove", (e) => {
+cardContainer.addEventListener("mousemove", (e) => {
   let yAxis = (window.innerWidth / 2 - e.pageY) / 25;
   let xAxis = (window.innerHeight / 2 - e.pageX) / 25;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 //Animate In
-container.addEventListener("mouseenter", (e) => {
+cardContainer.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
   //Popout
   title.style.transform = "translateZ(150px)";
-  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  photoContainer.style.transform = "translateZ(200px) rotateZ(-45deg)";
   description.style.transform = "translateZ(125px)";
-  sizes.style.transform = "translateZ(100px)";
-  purchase.style.transform = "translateZ(75px)";
+  cta.style.transform = "translateZ(75px)";
 });
 //Animate Out
-container.addEventListener("mouseleave", (e) => {
+cardContainer.addEventListener("mouseleave", (e) => {
   card.style.transition = "all 0.5s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
   //Popback
   title.style.transform = "translateZ(0px)";
-  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  photoContainer.style.transform = "translateZ(0px) rotateZ(0deg)";
   description.style.transform = "translateZ(0px)";
-  sizes.style.transform = "translateZ(0px)";
-  purchase.style.transform = "translateZ(0px)";
+  cta.style.transform = "translateZ(0px)";
 });
